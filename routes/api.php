bb87811fc0306e43e7aa66bd67bdeb60071bd813
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-09-27 20:12:33 -0300

    feat(auth): creating middlewares Admin and SuperAdmin

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/servidores', [UserController::class, 'index']);
-Route::get('/servidor/{matriculaSiape}', [UserController::class, 'show']);
-Route::put('/servidor/{matriculaSiape}', [UserController::class, 'update']);
-Route::delete('/servidor/{matriculaSiape}', [UserController::class, 'delete']);
+
 Route::get('/servidor/{matriculaSiape}/minhasPortarias', [UserController::class, 'minhasPortarias']);
-
-Route::get('/portarias', [PortariaController::class, 'index']);
-Route::get('/portaria/{numeroPortaria}', [PortariaController::class, 'show']);
-Route::get('/portaria/{numeroPortaria}/documento', [PortariaController::class, 'getDocumento']);
-Route::post('/portaria', [PortariaController::class, 'store']);
-Route::put('/portaria/{numeroPortaria}', [PortariaController::class, 'update']);
-Route::delete('/portaria/{numeroPortaria}', [PortariaController::class, 'delete']);
-
 
 Route::get('/relatorio/ranking', [RelatoriosController::class, 'ranking']);
 Route::get('/relatorio/portarias/publicas', [RelatoriosController::class, 'portariasPublicas']);
 Route::get('/relatorio/portarias/privadas', [RelatoriosController::class, 'portariasPrivadas']);
 Route::get('/relatorio/portarias/permanentes', [RelatoriosController::class, 'portariasPermanentes']);
+
+
+
+Route::middleware(['admin', 'superAdmin'])->group(function () {
+
+    Route::get('/servidores', [UserController::class, 'index']);
+    Route::get('/servidor/{matriculaSiape}', [UserController::class, 'show']);
+    Route::put('/servidor/{matriculaSiape}', [UserController::class, 'update']);
+    Route::delete('/servidor/{matriculaSiape}', [UserController::class, 'delete']);
+
+    Route::get('/portarias', [PortariaController::class, 'index']);
+    Route::get('/portaria/{numeroPortaria}', [PortariaController::class, 'show']);
+    Route::get('/portaria/{numeroPortaria}/documento', [PortariaController::class, 'getDocumento']);
+    Route::post('/portaria', [PortariaController::class, 'store']);
+    Route::put('/portaria/{numeroPortaria}', [PortariaController::class, 'update']);
+    Route::delete('/portaria/{numeroPortaria}', [PortariaController::class, 'delete']);
+});
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
