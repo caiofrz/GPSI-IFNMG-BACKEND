6f3d201020560704793bc5010c0a0f9ea0a02ee6
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-09-29 12:36:59 -0300

    fix(user): fixing validate user register

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
-
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\PortariaController;
 use App\Http\Controllers\RelatoriosController;
 use App\Http\Controllers\UserController;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,10 +29,11 @@ Route::get('/relatorio/portarias/permanentes', [RelatoriosController::class, 'po
 
 
 
-Route::middleware(['admin', 'superAdmin'])->group(function () {
+// Route::middleware(['admin', 'superAdmin'])->group(function () {
 
     Route::get('/servidores', [UserController::class, 'index']);
     Route::get('/servidor/{matriculaSiape}', [UserController::class, 'show']);
+    Route::post('/servidor', [RegisteredUserController::class, 'store']);
     Route::put('/servidor/{matriculaSiape}', [UserController::class, 'update']);
     Route::delete('/servidor/{matriculaSiape}', [UserController::class, 'delete']);
 
@@ -40,7 +43,7 @@ Route::middleware(['admin', 'superAdmin'])->group(function () {
     Route::post('/portaria', [PortariaController::class, 'store']);
     Route::put('/portaria/{numeroPortaria}', [PortariaController::class, 'update']);
     Route::delete('/portaria/{numeroPortaria}', [PortariaController::class, 'delete']);
-});
+// });
 
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
